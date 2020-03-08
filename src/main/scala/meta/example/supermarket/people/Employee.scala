9190cf7a5e437a08d196f9ac3edc1d367d094c30
ramezanikebrya <hamid.ramezani1375@gmail.com>
2020-03-08 16:47:31 +0100

    fix some bugs for employee

--- src/main/scala/meta/example/supermarket/people/Employee.scala
+++ src/main/scala/meta/example/supermarket/people/Employee.scala
@@ -23,98 +23,51 @@ class Employee extends Actor {
         Supermarket.store.warehouse(item) += (new_item.asInstanceOf[Item])
         println("Add new actor! name: " + item)
       })
-      //      while (i < fillShelf(item)) {
-      //        val new_item: Item = genNewItem(newItemsMap.itemMap(item))
-      //        Supermarket.store.warehouse(item) += (new_item.asInstanceOf[Item])
-      //        println("Add new actor! name: " + item)
-      //        i = i + 1
-      //      }
     )
   }
 
   def genNewItem(itemId: String): Item = {
-    //    itemId match {
     var lstOfNewItems: List[Item] = List()
-
-    lstOfNewItems = lstOfNewItems.::(new Item32())
-    lstOfNewItems = lstOfNewItems.::(new Item31())
-    lstOfNewItems = lstOfNewItems.::(new Item30())
-    lstOfNewItems = lstOfNewItems.::(new Item29())
-    lstOfNewItems = lstOfNewItems.::(new Item28())
-    lstOfNewItems = lstOfNewItems.::(new Item27())
-    lstOfNewItems = lstOfNewItems.::(new Item26())
-    lstOfNewItems = lstOfNewItems.::(new Item25())
-    lstOfNewItems = lstOfNewItems.::(new Item24())
-    lstOfNewItems = lstOfNewItems.::(new Item23())
-    lstOfNewItems = lstOfNewItems.::(new Item22())
-    lstOfNewItems = lstOfNewItems.::(new Item21())
-    lstOfNewItems = lstOfNewItems.::(new Item20())
-    lstOfNewItems = lstOfNewItems.::(new Item19())
-    lstOfNewItems = lstOfNewItems.::(new Item18())
-    lstOfNewItems = lstOfNewItems.::(new Item17())
-    lstOfNewItems = lstOfNewItems.::(new Item16())
-    lstOfNewItems = lstOfNewItems.::(new Item15())
-    lstOfNewItems = lstOfNewItems.::(new Item14())
-    lstOfNewItems = lstOfNewItems.::(new Item13())
-    lstOfNewItems = lstOfNewItems.::(new Item12())
-    lstOfNewItems = lstOfNewItems.::(new Item11())
-    lstOfNewItems = lstOfNewItems.::(new Item10())
-    lstOfNewItems = lstOfNewItems.::(new Item9())
-    lstOfNewItems = lstOfNewItems.::(new Item8())
-    lstOfNewItems = lstOfNewItems.::(new Item7())
-    lstOfNewItems = lstOfNewItems.::(new Item6())
-    lstOfNewItems = lstOfNewItems.::(new Item5())
-    lstOfNewItems = lstOfNewItems.::(new Item4())
-    lstOfNewItems = lstOfNewItems.::(new Item3())
-    lstOfNewItems = lstOfNewItems.::(new Item2())
-    lstOfNewItems = lstOfNewItems.::(new Item1())
-
-    var index: Int = Integer.parseInt(itemId.replaceAll("Item",""))
-
-//    var index: Int = 5
-
-
-    lstOfNewItems.apply(index-1)
-
-    //      case "Item1" => new Item1()
-    //      case "Item2" => new Item2()
-    //      case "Item3" => new Item3()
-    //      case "Item4" => new Item4()
-    //      case "Item5" => new Item5()
-    //      case "Item6" => new Item6()
-    //      case "Item7" => new Item7()
-    //      case "Item8" => new Item8()
-    //      case "Item9" => new Item9()
-    //      case "Item10" => new Item10()
-    //      case "Item11" => new Item11()
-    //      case "Item12" => new Item12()
-    //      case "Item13" => new Item13()
-    //      case "Item14" => new Item14()
-    //      case "Item15" => new Item15()
-    //      case "Item16" => new Item16()
-    //      case "Item17" => new Item17()
-    //      case "Item18" => new Item18()
-    //      case "Item19" => new Item19()
-    //      case "Item20" => new Item20()
-    //      case "Item21" => new Item21()
-    //      case "Item22" => new Item22()
-    //      case "Item23" => new Item23()
-    //      case "Item24" => new Item24()
-    //      case "Item25" => new Item25()
-    //      case "Item26" => new Item26()
-    //      case "Item27" => new Item27()
-    //      case "Item28" => new Item28()
-    //      case "Item29" => new Item29()
-    //      case "Item30" => new Item30()
-    //      case "Item31" => new Item31()
-    //      case "Item32" => new Item32()
-    //      case _ => throw new IllegalArgumentException
-    //    }
+    var index: Int = Integer.parseInt(itemId.replaceAll("Item", ""))
+    if (index == 32) new Item32()
+    else if (index == 31) new Item31()
+    else if (index == 30) new Item30()
+    else if (index == 29) new Item29()
+    else if (index == 28) new Item28()
+    else if (index == 27) new Item27()
+    else if (index == 26) new Item26()
+    else if (index == 25) new Item25()
+    else if (index == 24) new Item24()
+    else if (index == 23) new Item23()
+    else if (index == 22) new Item22()
+    else if (index == 21) new Item21()
+    else if (index == 20) new Item20()
+    else if (index == 19) new Item19()
+    else if (index == 18) new Item18()
+    else if (index == 17) new Item17()
+    else if (index == 16) new Item16()
+    else if (index == 15) new Item15()
+    else if (index == 14) new Item14()
+    else if (index == 13) new Item13()
+    else if (index == 12) new Item12()
+    else if (index == 11) new Item11()
+    else if (index == 10) new Item10()
+    else if (index == 9) new Item9()
+    else if (index == 8) new Item8()
+    else if (index == 7) new Item7()
+    else if (index == 6) new Item6()
+    else if (index == 5) new Item5()
+    else if (index == 4) new Item4()
+    else if (index == 3) new Item3()
+    else if (index == 2) new Item2()
+    else if (index == 1) new Item1()
+    else new Item1
+    //TODO : exception handling cannot be lifted. fix this later.
+    //    else throw new IllegalArgumentException
   }
 
   def main(): Unit = {
     while (true) {
-      //      Supermarket.store.addSupply
       addSupply
       waitTurns(1)
     }
