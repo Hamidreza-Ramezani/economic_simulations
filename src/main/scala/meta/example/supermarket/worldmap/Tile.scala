ef5d0fbbb4fed9b3a961b0997ded4595dbdb572f
ramezanikebrya <hamid.ramezani1375@gmail.com>
2020-06-07 22:43:24 +0200

    wip: add tiled map

---
+++ src/main/scala/meta/example/supermarket/worldmap/Tile.scala
@@ -0,0 +1,23 @@
+package meta.example.supermarket.worldmap
+
+import scala.collection.mutable.ListBuffer
+//import scala.tools.nsc.doc.model.Entity
+
+class Tile (xPos: Int, yPos: Int) {
+  var  typee: String
+
+   var entities: ListBuffer[Entity] = new ListBuffer[Entity]
+
+  def setType(typee: String ): Unit = {
+    this.typee = typee;
+  }
+  def  getType(): String =  {
+    return type;
+  }
+  def addEntity(e: Entity): Unit = {
+    entities.add(e);
+  }
+  def removeEntity(e: Entity ): Unit = {
+    entities.remove(e);
+  }
+}
