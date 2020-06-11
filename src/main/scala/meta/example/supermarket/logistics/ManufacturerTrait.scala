2f8435acc37e41f89ba120bc7720c41458fdeca6
ramezanikebrya <hamid.ramezani1375@gmail.com>
2020-06-11 23:03:16 +0200

    wip logistics

--- src/main/scala/meta/example/supermarket/logistics/ManufacturerTrait.scala
+++ src/main/scala/meta/example/supermarket/logistics/ManufacturerTrait.scala
@@ -23,41 +23,4 @@ trait ManufacturerTrait extends Actor{
     "Carrots" -> new mutable.Queue[Item],
     "Mushroom" -> new mutable.Queue[Item]
   )
-
-  //  var storage: mutable.Map[String, mutable.Queue[Item]] = mutable.Map(
-//    "Yogurt" -> new mutable.Queue[Item],
-//    "Squash" -> new mutable.Queue[Item],
-//    "Bacon" -> new mutable.Queue[Item],
-//    "Cheese" -> new mutable.Queue[Item],
-//    "Ferraro" -> new mutable.Queue[Item],
-//    "Oatmeal" -> new mutable.Queue[Item],
-//    "Cabbage" -> new mutable.Queue[Item],
-//    "Beef" -> new mutable.Queue[Item],
-//    "Broccoli" -> new mutable.Queue[Item],
-//    "Noodles" -> new mutable.Queue[Item],
-//    "Eggplant" -> new mutable.Queue[Item],
-//    "Potato" -> new mutable.Queue[Item],
-//    "Celery" -> new mutable.Queue[Item],
-//    "Kitkat" -> new mutable.Queue[Item],
-//    "Pasta" -> new mutable.Queue[Item],
-//    "Cucumber" -> new mutable.Queue[Item],
-//    "Tomato" -> new mutable.Queue[Item],
-//    "Cereal" -> new mutable.Queue[Item],
-//    "Rice" -> new mutable.Queue[Item],
-//    "DarkChocolate" -> new mutable.Queue[Item],
-//    "Onion" -> new mutable.Queue[Item],
-//    "Carrots" -> new mutable.Queue[Item],
-//    "Cream" -> new mutable.Queue[Item],
-//    "Lamb" -> new mutable.Queue[Item],
-//    "WhiteChocolate" -> new mutable.Queue[Item],
-//    "Bread" -> new mutable.Queue[Item],
-//    "Pork" -> new mutable.Queue[Item],
-//    "Mushroom" -> new mutable.Queue[Item],
-//    "Spaghetti" -> new mutable.Queue[Item],
-//    "Egg" -> new mutable.Queue[Item],
-//    "Milk" -> new mutable.Queue[Item],
-//    "Chicken" -> new mutable.Queue[Item]
-//  )
-
-
 }
